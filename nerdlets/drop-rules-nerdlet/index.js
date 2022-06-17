fcbf4c2a278c97798c00e5e7808bb708289f18a6
James DeZego <james.dezego@gmail.com>
2022-06-17 12:20:20 -0400

    whitespace cleanup

--- nerdlets/drop-rules-nerdlet/index.js
+++ nerdlets/drop-rules-nerdlet/index.js
@@ -26,55 +26,55 @@ export default class DropRules extends React.Component {
         return (
             // Using this NerdGraphQuery component for testing. Not sure if it is suitable for this project.
             // Ultimately want to display rules in a NR React Table component.
-            <NerdGraphQuery query={ListDropRules(accountId)}>
-                {({ loading, error, data }) => {
-                    if (loading) {
-                        return <Spinner />
-                    }
+          <NerdGraphQuery query={ListDropRules(accountId)}>
+              {({ loading, error, data }) => {
+                  if (loading) {
+                      return <Spinner />
+                  }
 
-                    if (error) {
-                        return "Error!"
-                    }
+                  if (error) {
+                      return "Error!"
+                  }
 
-                    // No rules found if length of rules array is zero.
-                    if (data.actor.account.nrqlDropRules.list.rules.length == 0) {
-                        return <h2>No rules found.</h2>
-                    }
+                  // No rules found if length of rules array is zero.
+                  if (data.actor.account.nrqlDropRules.list.rules.length == 0) {
+                      return <h2>No rules found.</h2>
+                  }
 
-                    // TODO:
-                    // Use NR React "Table" component to display rules.
-                    // Include Table header action "Delete" to delete a rule instead of using Button.
-                    // Reference: https://developer.newrelic.com/components/table
-                    return (
-                      <>
-                        <div>
-                        <h3>accountId from dropdown: <GetAccountId /></h3>
-                            {data.actor.account.nrqlDropRules.list.rules.map((rule) => (
-                                  <h3>
-                                    <p>&nbsp;</p>
-                                    <p key={rule.nrql}>Rule: {rule.nrql}</p>
-                                    <p key={rule.description}>Description: {rule.description}</p>
-                                    <p key={rule.creator.email}>Creator: {rule.creator.email}</p>
-                                    <p><Button type={Button.TYPE.DESTRUCTIVE}>Delete Drop Rule</Button></p>
-                                  </h3>
-                                )
-                            )}
-                        </div>
-                        <div>
-                          <p>&nbsp;</p>
-                          <p>&nbsp;</p>
-                          <p>&nbsp;</p>
-                          <h2>Create Drop Rule</h2>
-                          <Form>
-                            <TextField placeholder="" label="NRQL" />
-                            <TextField placeholder="" label="Description" />
-                            <Button type={Button.TYPE.PRIMARY}>Create Drop Rule</Button>
-                          </Form>
-                        </div>
-                      </>
-                    )
-                }}
-            </NerdGraphQuery>
+                  // TODO:
+                  // Use NR React "Table" component to display rules.
+                  // Include Table header action "Delete" to delete a rule instead of using Button.
+                  // Reference: https://developer.newrelic.com/components/table
+                  return (
+                    <>
+                      <div>
+                      <h3>accountId from dropdown: <GetAccountId /></h3>
+                          {data.actor.account.nrqlDropRules.list.rules.map((rule) => (
+                                <h3>
+                                  <p>&nbsp;</p>
+                                  <p key={rule.nrql}>Rule: {rule.nrql}</p>
+                                  <p key={rule.description}>Description: {rule.description}</p>
+                                  <p key={rule.creator.email}>Creator: {rule.creator.email}</p>
+                                  <p><Button type={Button.TYPE.DESTRUCTIVE}>Delete Drop Rule</Button></p>
+                                </h3>
+                              )
+                          )}
+                      </div>
+                      <div>
+                        <p>&nbsp;</p>
+                        <p>&nbsp;</p>
+                        <p>&nbsp;</p>
+                        <h2>Create Drop Rule</h2>
+                        <Form>
+                          <TextField placeholder="" label="NRQL" />
+                          <TextField placeholder="" label="Description" />
+                          <Button type={Button.TYPE.PRIMARY}>Create Drop Rule</Button>
+                        </Form>
+                      </div>
+                    </>
+                  )
+              }}
+          </NerdGraphQuery>
         )
     }
 }
